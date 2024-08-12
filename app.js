aceb1fb8505a1c40d3cbb57d96bd671e32786a23
yasminnur <yasminnurazmina@gmail.com>
2024-08-12 16:29:37 +0700

    update

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-console.log("Script loaded"); // Cek apakah script berjalan
 let items = document.querySelectorAll(`.slider .card-item`);
 let next = document.getElementById(`next`);
 let prev = document.getElementById(`prev`);
@@ -7,26 +6,44 @@ let active = 3;
 function loadShow() {
     let stt = 0;
     items[active].style.transform = `none`;
-    // items[i].style.zIndex = 1;
     items[active].style.filter = `none`;
     items[active].style.opacity = 1;
+    items[active].style.top = "25%";
+    items[active].style.zIndex = items.length;
     for (var i = active + 1; i < items.length; i++){
         stt++;
-        items[i].style.transform = `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`;
-        // items[i].style.zIndex = -stt;
-        // items[i].style.zIndex = stt;
-        items[i].style.filter = `blur(5px)`;
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        items[i].style.opacity = stt > 2 ? 0 : 1;
+        items[i].style.zIndex = items.length - stt;
+        console.log(`Card ${i} zIndex: `, items[i].style.zIndex)
+
+        if (i === active + 1) {
+            items[i].style.transformOrigin += "top left";
+            items[i].style.transform += `translateX(${270 * stt}px) translateY(50%) rotate(-15deg)`;
+            items[i].style.color += 'red';
+        } else {
+            items[i].style.transform = `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`;
+            items[i].style.color += 'blue';
+        }
     }
     stt = 0;
     for (var i = active - 1; i >= 0; i--){
         stt++;
-        items[i].style.transform = `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`;
-        // items[i].style.zIndex = -stt;
-        items[i].style.filter = `blur(5px)`;
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        items[i].style.opacity = stt > 2 ? 0 : 1;
+        items[i].style.zIndex = items.length - stt;
+
+        if (i === active - 1) {
+            items[i].style.transformOrigin += "bottom right";
+            items[i].style.transform += `translateX(${-270 * stt}px) translateY(-50%) rotate(15deg)`;
+            items[i].style.color += 'red';
+        } else {
+            items[i].style.transform = `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`;
+            items[i].style.color += 'blue';
+        }
     }
+
+    
 }
+
 loadShow();
 
 next.onclick = function () {
